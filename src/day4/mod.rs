6d9417650f08f69edbdfe8b286da1631d3c7f607
Doug Aaser <douglasaaser@gmail.com>
2022-12-04 06:13:51 -0500

    Move re-used code to function

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -2,13 +2,16 @@ use std::collections::HashSet;
 
 const INPUT: &'static str = include_str!("input.txt");
 
+fn parse_line_to_set(line: &str) -> (HashSet<usize>, HashSet<usize>) {
+  let range: Vec<usize> = line.split(&[',', '-']).map(|n| n.parse::<usize>().unwrap()).collect();
+  let [r1_start, r1_end, r2_start, r2_end] = range[..] else { unreachable!() };
+  ((r1_start..=r1_end).collect(), (r2_start..=r2_end).collect())
+}
+
 pub fn part1() -> usize {
   let mut total = 0usize;
   for line in INPUT.lines() {
-    let range: Vec<usize> = line.split(&[',', '-']).map(|n| n.parse::<usize>().unwrap()).collect();
-    let [r1_start, r1_end, r2_start, r2_end] = range[..] else { unreachable!() };
-    let r1: HashSet<usize> = (r1_start..=r1_end).collect();
-    let r2: HashSet<usize> = (r2_start..=r2_end).collect();
+    let (r1, r2) = parse_line_to_set(line);
     if r1.is_subset(&r2) || r2.is_subset(&r1) {
       total += 1;
     }
@@ -19,10 +22,7 @@ pub fn part1() -> usize {
 pub fn part2() -> usize {
   let mut total = 0usize;
   for line in INPUT.lines() {
-    let range: Vec<usize> = line.split(&[',', '-']).map(|n| n.parse::<usize>().unwrap()).collect();
-    let [r1_start, r1_end, r2_start, r2_end] = range[..] else { unreachable!() };
-    let r1: HashSet<usize> = (r1_start..=r1_end).collect();
-    let r2: HashSet<usize> = (r2_start..=r2_end).collect();
+    let (r1, r2) = parse_line_to_set(line);
     if !r1.is_disjoint(&r2) {
       total += 1;
     }
