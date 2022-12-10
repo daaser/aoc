eab97e4e24cb753d311d398f92a8429ea9a49926
Doug Aaser <douglasaaser@gmail.com>
2022-12-10 14:53:57 -0500

    Massively clean up logic

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -12,7 +12,6 @@ enum Inst {
 #[derive(Debug)]
 struct Program {
   ops: Vec<Inst>,
-  cycle: isize,
   x: isize,
 }
 
@@ -22,79 +21,50 @@ impl FromStr for Program {
   fn from_str(s: &str) -> Result<Self, Self::Err> {
     let mut ops = vec![];
     for line in s.lines() {
-      ops.push(if line.starts_with("noop") {
-        Inst::Noop
-      } else {
-        line
-          .split_once(' ')
-          .map(|(_, val)| {
-            let val = val.parse::<isize>().unwrap();
-            Inst::Addx(val)
-          })
-          .unwrap()
-      })
+      ops.push(Inst::Noop);
+      if !line.starts_with("noop") {
+        ops.push(
+          line
+            .split_once(' ')
+            .map(|(_, val)| {
+              let val = val.parse::<isize>().unwrap();
+              Inst::Addx(val)
+            })
+            .unwrap(),
+        );
+      }
     }
-    Ok(Self { ops, x: 1, cycle: 1 })
+    Ok(Self { ops, x: 1 })
   }
 }
 
 impl Program {
   fn signal_strength(&mut self) -> isize {
     let mut signal_strength: isize = 0;
-    for op in self.ops.iter() {
-      match op {
-        Inst::Addx(val) => {
-          for i in 0..2 {
-            if CYCLES.contains(&self.cycle) {
-              signal_strength += self.x * self.cycle;
-            }
-            self.cycle += 1;
-            if i == 1 {
-              // after cycle
-              self.x += val;
-            }
-          }
-        }
-        Inst::Noop => {
-          if CYCLES.contains(&self.cycle) {
-            signal_strength += self.x * self.cycle;
-          }
-          self.cycle += 1;
-        }
+    for (cycle, op) in self.ops.iter().enumerate() {
+      let cycle = cycle as isize + 1;
+      if CYCLES.contains(&cycle) {
+        signal_strength += self.x * cycle as isize;
+      }
+      if let Inst::Addx(val) = op {
+        self.x += val;
       }
     }
-
     signal_strength
-  }
-
-  fn update_screen(&self, screen: &mut [[bool; 40]; 6]) {
-    let cycle = self.cycle - 1;
-    let positions = [self.x - 1, self.x, self.x + 1];
-
-    let row = cycle / 40isize;
-    let col = cycle % 40isize;
-
-    screen[row as usize][col as usize] = positions.contains(&col);
   }
 
   fn render_screen(&mut self) {
     let mut screen = [[false; 40]; 6];
-    for op in self.ops.iter() {
-      match op {
-        Inst::Addx(val) => {
-          for i in 0..2 {
-            self.update_screen(&mut screen);
-            self.cycle += 1;
-            if i == 1 {
-              // after cycle
-              self.x += val;
-            }
-          }
-        }
-        Inst::Noop => {
-          self.update_screen(&mut screen);
-          self.cycle += 1;
-        }
+    for (cycle, op) in self.ops.iter().enumerate() {
+      let positions = [self.x - 1, self.x, self.x + 1];
+
+      let row = cycle / 40;
+      let col = cycle % 40;
+
+      screen[row][col] = positions.contains(&(col as isize));
+
+      if let Inst::Addx(val) = op {
+        self.x += val;
       }
     }
 
